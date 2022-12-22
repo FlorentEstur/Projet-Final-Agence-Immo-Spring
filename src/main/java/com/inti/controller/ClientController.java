93b266e402d10304eee24d4021c3a6738a347e93
Drozerd <debeneyf@gmail.com>
2022-12-22 14:34:11 +0100

    Ebauche fonction recherche avancée

--- src/main/java/com/inti/controller/ClientController.java
+++ src/main/java/com/inti/controller/ClientController.java
@@ -1,33 +1,105 @@
 package com.inti.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Achat;
 import com.inti.model.Client;
 import com.inti.model.Location;
+import com.inti.repository.IAchatRepository;
+import com.inti.repository.ILocationRepository;
 
 @RestController
 public class ClientController {
 	
-//	@Autowired
-//	ProducerTemplate producerTemplate;
-//	
-//	
-//	@GetMapping("/getOffreAchat/{id}")
-//	public Achat getOffreAchat(@PathVariable int id) 
-//	{
-//		return producerTemplate.requestBody("direct:selectAchat", null, Achat.class);
-//	}
-//	
-//	@GetMapping("/getOffreLocation/{id}")
-//	public Location getOffreLocation(@PathVariable int id) 
-//	{
-//		return producerTemplate.requestBody("direct:selectLocation", null, Location.class);
-//	}
-//	
+	
+	@Autowired
+	IAchatRepository iAchatRepository;
+	
+	@Autowired
+	ILocationRepository iLocationRepository;
+	
+	
+	//LISTES
+	@GetMapping("listeAchatsClient")
+	public List<Achat> listeAchats()
+	{
+		return iAchatRepository.findAll();
+	}
+	
+	@GetMapping("listeLocationsClient")
+	public List<Location> listeLocations()
+	{
+		return iLocationRepository.findAll();
+	}
+	
+	//SELECTION DE 1 OFFRE OU LOC
+		@GetMapping("/getAchatClient/{id}")
+		public Achat getAchat (@PathVariable int id) 
+		{
+			try {
+				return iAchatRepository.findById(id).get();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@GetMapping("/getLocationClient/{id}")
+		public Location getLocation(@PathVariable int id) 
+		{
+			try {
+				return iLocationRepository.findById(id).get();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		//COMPARAISON
+		@GetMapping("/compareAchat/{id}/{id2}")
+		public List<Achat> comparaisonAchat(@PathVariable int id, @PathVariable int id2) 
+		{
+				Achat achat1 = iAchatRepository.findById(id).get();
+				Achat achat2 = iAchatRepository.findById(id2).get();
+				List<Achat> listeachAchats = new ArrayList<>();
+				listeachAchats.add(achat1);
+				listeachAchats.add(achat2);
+				return listeachAchats;			
+		}
+		
+		@GetMapping("/compareLocation/{id}/{id2}")
+		public List<Location> comparaisonLocation(@PathVariable int id, @PathVariable int id2) 
+		{
+				Location location1 = iLocationRepository.findById(id).get();
+				Location location2 = iLocationRepository.findById(id2).get();
+				List<Location> listeLocation = new ArrayList<>();
+				listeLocation.add(location1);
+				listeLocation.add(location2);
+				return listeLocation;			
+		}
+		
+		//RECHERCHE AVANCEE
+		
+		@GetMapping("/rechercheAchat")
+		public List<Achat> rechercheAchat (@RequestParam int id, @RequestParam String adresse, @RequestParam String description, @RequestParam int piece, @RequestParam int chambre, @RequestParam double prixAchat, @RequestParam double surface,@RequestParam boolean ascenceur, @RequestParam boolean parking) 
+		{ 
+			return iAchatRepository.rechercheAchat(id, adresse, description, piece, chambre, prixAchat, surface, ascenceur, parking);
+		}
+		
+		
+		
+		
+	
+		
+		
+	
 	
 }
--- src/main/java/com/inti/repository/IAchatRepository.java
+++ src/main/java/com/inti/repository/IAchatRepository.java
@@ -1,10 +1,18 @@
 package com.inti.repository;
 
 import com.inti.model.Achat;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAchatRepository extends JpaRepository<Achat, Integer>{
+	
+	@Query(value = "select * from Achat_Projet where id:=id and adresse:=adresse and description:=description and piece:=piece and chambre:=chambre and prixAchat:=prixAchat and surface:=surface and ascenceur:=ascenceur and parking:=parking", nativeQuery = true)
+	List<Achat> rechercheAchat(@Param ("id") int id, @Param ("adresse") String adresse, @Param ("description") String description, @Param ("piece") int piece, @Param ("chambre") int chambre, @Param ("prixAchat") double prixAchat, @Param ("surface") double surface, @Param ("ascenceur") boolean ascenceur, @Param ("parking") boolean parking);
 
 }
