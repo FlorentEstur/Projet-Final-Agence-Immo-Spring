0b5e4956002497b5d0dd7c3f282835b6a83791c9
Drozerd <debeneyf@gmail.com>
2022-12-22 17:06:57 +0100

    Toutes les focntions achats cote client

--- src/main/java/com/inti/controller/ClientController.java
+++ src/main/java/com/inti/controller/ClientController.java
@@ -3,7 +3,7 @@ package com.inti.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.camel.ProducerTemplate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.model.Achat;
-import com.inti.model.Client;
 import com.inti.model.Location;
 import com.inti.repository.IAchatRepository;
 import com.inti.repository.ILocationRepository;
@@ -89,10 +88,29 @@ public class ClientController {
 		//RECHERCHE AVANCEE
 		
 		@GetMapping("/rechercheAchat")
-		public List<Achat> rechercheAchat (@RequestParam int id, @RequestParam String adresse, @RequestParam String description, @RequestParam int piece, @RequestParam int chambre, @RequestParam double prixAchat, @RequestParam double surface,@RequestParam boolean ascenceur, @RequestParam boolean parking) 
+		public List<Achat> rechercheAchat (@RequestParam int id, @RequestParam String adresse, @RequestParam String description, @RequestParam int piece, @RequestParam int chambre, @RequestParam double prix_Achat, @RequestParam double surface,@RequestParam boolean ascenceur, @RequestParam boolean parking) 
 		{ 
-			return iAchatRepository.rechercheAchat(id, adresse, description, piece, chambre, prixAchat, surface, ascenceur, parking);
+			return iAchatRepository.rechercheAchat(id, adresse, description, piece, chambre, prix_Achat, surface, ascenceur, parking);
 		}
+		
+		@GetMapping("/rechercheAchatVille")
+		public List<Achat> rechercheAchatVille( @RequestParam String adresse)
+		{
+			return iAchatRepository.rechercheAchatVille(adresse);
+		}
+		
+		@GetMapping("/rechercheAchatVilleType")
+		public List<Achat> rechercheAchatVilleType( @RequestParam String adresse, @RequestParam String description)
+		{
+			return iAchatRepository.rechercheAchatVilleType(adresse, description);
+		}
+		
+		@GetMapping("/rechercheAchatPieceChambre")
+		public List<Achat> rechercheAchatPieceChambre( @RequestParam int piece, @RequestParam int chambre)
+		{
+			return iAchatRepository.rechercheAchatPieceChambre(piece, chambre);
+		}
+		
 		
 		
 		
--- src/main/java/com/inti/repository/IAchatRepository.java
+++ src/main/java/com/inti/repository/IAchatRepository.java
@@ -12,7 +12,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IAchatRepository extends JpaRepository<Achat, Integer>{
 	
-	@Query(value = "select * from Achat_Projet where id:=id and adresse:=adresse and description:=description and piece:=piece and chambre:=chambre and prixAchat:=prixAchat and surface:=surface and ascenceur:=ascenceur and parking:=parking", nativeQuery = true)
-	List<Achat> rechercheAchat(@Param ("id") int id, @Param ("adresse") String adresse, @Param ("description") String description, @Param ("piece") int piece, @Param ("chambre") int chambre, @Param ("prixAchat") double prixAchat, @Param ("surface") double surface, @Param ("ascenceur") boolean ascenceur, @Param ("parking") boolean parking);
+	@Query(value = "select * from Achat_Projet where id=:id and adresse=:adresse and description=:description and piece=:piece and chambre=:chambre and prix_achat=:prix_achat and surface=:surface and ascenceur=:ascenceur and parking=:parking", nativeQuery = true)
+	List<Achat> rechercheAchat(@Param ("id") int id, @Param ("adresse") String adresse, @Param ("description") String description, @Param ("piece") int piece, @Param ("chambre") int chambre, @Param ("prix_achat") double prix_Achat, @Param ("surface") double surface, @Param ("ascenceur") boolean ascenceur, @Param ("parking") boolean parking);
 
+	@Query(value = "select * from Achat_Projet where  adresse=:adresse", nativeQuery = true)
+	List<Achat> rechercheAchatVille(@Param ("adresse") String adresse);
+	
+	@Query(value = "select * from Achat_Projet where  adresse=:adresse and description=:description", nativeQuery = true)
+	List<Achat> rechercheAchatVilleType(@Param ("adresse") String adresse, @Param ("description") String description);
+	
+	@Query(value = "select * from Achat_Projet where  piece=:piece and chambre=:chambre", nativeQuery = true)
+	List<Achat> rechercheAchatPieceChambre(@Param ("piece") int piece,  @Param ("chambre") int chambre);
+	
 }
